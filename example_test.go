a6c99707f22814e5211d66e018b9e97b1991792d
Arthur White <arthur@whitedevops.com>
2016-03-27 21:58:49 +0200

    Add examples

---
+++ example_test.go
@@ -0,0 +1,36 @@
+package compress_test
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gowww/compress"
+)
+
+func Example() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response is gzipped when content is long enough.")
+	})
+
+	http.ListenAndServe(":8080", compress.Handle(mux))
+}
+
+func ExampleHandle() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response is gzipped when content is long enough.")
+	})
+
+	http.ListenAndServe(":8080", compress.Handle(mux))
+}
+
+func ExampleHandleFunc() {
+	http.Handle("/", compress.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response is gzipped when content is long enough.")
+	}))
+
+	http.ListenAndServe(":8080", nil)
+}
